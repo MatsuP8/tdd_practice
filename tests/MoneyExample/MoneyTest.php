8eea720fd14a79696c5a5d4a530fa67d777515b6
MatsuP8 <matsumoto.s.s.0805@gmail.com>
2020-01-08 07:10:34 +0900

    サブクラスを削除した。 冗長なテストを削除した。

--- tests/MoneyExample/MoneyTest.php
+++ tests/MoneyExample/MoneyTest.php
@@ -4,7 +4,6 @@
 namespace Test\MoneyExample;
 
 
-use App\MoneyExample\Franc;
 use App\MoneyExample\Money;
 use PHPUnit\Framework\TestCase;
 
@@ -25,30 +24,12 @@ class MoneyTest extends TestCase
 
         $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
 
-        $this->assertTrue(Money::franc(5)->equals(Money::franc(5)));
-
-        $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
-
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
-    }
-
-    public function testFrancMultiplication()
-    {
-        $franc = Money::franc(5);
-
-        $this->assertTrue(Money::franc(10)->equals($franc->times(2)));
-
-        $this->assertTrue(Money::franc(15)->equals($franc->times(3)));
     }
 
     public function testCurrency()
     {
         $this->assertEquals("USD", Money::dollar(1)->currency());
         $this->assertEquals("CHF", Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, "CTF"))->equals(new Franc(10, "CTF")));
     }
 }
